19cfe00b5ad730e4e1291f6ee51c4b509dd1eb20
wuyachen <1774288078@qq.com>
2021-01-27 22:53:26 -0500

    merge change

--- codebee/src/App.js
+++ codebee/src/App.js
@@ -5,13 +5,14 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Login from "./component/login";
 import SignUp from "./component/signup";
+import Home from "./component/home"
 
 function App() {
   return (<Router>
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
         <div className="container">
-          <Link className="navbar-brand" to={"/sign-in"}>Home</Link>
+          <Link className="navbar-brand" to={"/"}>Home</Link>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
@@ -28,7 +29,7 @@ function App() {
       <div className="auth-wrapper">
         <div className="auth-inner">
           <Switch>
-            <Route exact path='/' component={Login} />
+            <Route exact path='/' component={Home} />
             <Route path="/sign-in" component={Login} />
             <Route path="/sign-up" component={SignUp} />
           </Switch>
