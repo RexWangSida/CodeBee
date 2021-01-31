e2f0bf14fce65d5693075a0d2f5c2d97f19d41b8
wuyachen <1774288078@qq.com>
2021-01-30 19:58:24 -0500

    merge changes

--- codebee/src/components/Home.js
+++ codebee/src/components/Home.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import HighlightedChar from "./HighlightedChar";
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-                <p>codebee</p>
+            <div className="flex flex-col mt-20">
+                <h1 className="md:text-4xl mb-16 text-5xl">
+                <HighlightedChar color="blue">C</HighlightedChar>odebee{" "}
+                </h1>
+                </div>
             </div>
         )}
 }
