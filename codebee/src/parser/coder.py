c44d83c5acea8929d39fd190d1a9b09f06689665
nagyj2 <jaysun_n@hotmail.com>
2021-02-06 15:53:25 -0500

    Simplified json coding logic

--- codebee/src/parser/coder.py
+++ codebee/src/parser/coder.py
@@ -13,15 +13,12 @@ class Decoder(json.JSONDecoder):
     _EXPRBLOCKS = ('literal', 'variable', 'binop', 'unop')
 
     def __init__(self, *args, **kwargs):
-        logging.info("Start Decode")
 
         for implemented in blocks.IMPLEMENTED:
             if implemented not in Decoder._STMTBLOCKS + Decoder._EXPRBLOCKS:
                 logging.warning('unimplemented JSON block: ' + str(implemented))
 
         json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
-
-        logging.info('Finished Decode')
 
     def object_hook(self, dct):
         result = None
@@ -41,96 +38,39 @@ class Decoder(json.JSONDecoder):
             logging.critical('Unknown element: ' + str(dct))
             sys.exit(1)
 
-        # Catch misassigned blocks - TODO make cleaner or remove when confident
-        if result.block == 'program' and type(result) != blocks.ProgramBlock:
-            logging.critical('Misassigned program block'); sys.exit(1)
-        elif result.block == 'scope' and type(result) != blocks.ScopeBlock:
-            logging.critical('Misassigned scope block'); sys.exit(1)
-        elif result.block == 'assignment' and type(result) != blocks.AssignmentBlock:
-            logging.critical('Misassigned assignment block'); sys.exit(1)
-        elif result.block == 'variable' and type(result) != blocks.VariableBlock:
-            logging.critical('Misassigned variable block'); sys.exit(1)
-        elif result.block == 'literal' and type(result) != blocks.LiteralBlock:
-            logging.critical('Misassigned literal block'); sys.exit(1)
-        elif result.block == 'binop' and type(result) != blocks.BinOpBlock:
-            logging.critical('Misassigned binop block'); sys.exit(1)
-        elif result.block == 'unop' and type(result) != blocks.UnOpBlock:
-            logging.critical('Misassigned unop block'); sys.exit(1)
-        else:
-            return result
-
-    @staticmethod
-    def _loadVariable(dct):
-        ident = dct['ident']
-
-        logging.debug('Finished Variable Block Load')
-        return blocks.VariableBlock(ident)
-
-    @staticmethod
-    def _loadLiteral(dct):
-        typ = dct['type']
-        value = dct['value']
-
-        logging.debug('Finished Literal Block Load')
-        return blocks.LiteralBlock(typ,value)
-
-    @staticmethod
-    def _loadBinOp(dct):
-        oper = dct['oper']
-        expr1 = dct['expr1']
-        expr2 = dct['expr2']
-
-        logging.debug('Finished BinOp Block Load')
-        return blocks.BinOpBlock(oper,expr1,expr2)
-
-    @staticmethod
-    def _loadUnOp(dct):
-        oper = dct['oper']
-        expr1 = dct['expr1']
-
-        logging.debug('Finished UnOp Block Load')
-        return blocks.UnOpBlock(oper,expr1)
-
-    @staticmethod
-    def _loadAssignment(dct):
-        ident = dct['ident'] # Decoder._loadVariable(dct['ident'])
-        expr = dct['expr'] # Decoder._loadExpr(dct['expr'])
-
-        logging.debug('Finished Assignment Block Load')
-        return blocks.AssignmentBlock(ident,expr)
-
-    @staticmethod
-    def _loadProgram(dct):
-        ident = dct['ident'] # Decoder._loadVariable(dct['ident'])
-        body = dct['body'] # Decoder._loadStmt(dct['body'])
-        param = None # Not implemented
-
-        logging.debug('Finished Program Block Load')
-        return blocks.ProgramBlock(ident,body,param)
-
-    @staticmethod
-    def _loadScope(dct):
-        stmts = dct['stmts'] # []
-        # for stmt in dct['stmts']:
-        #     stmts.append(Decoder._loadStmt(stmt))
-
-        logging.debug('Finished Scope Block Load')
-        return blocks.ScopeBlock(stmts)
-
+        return result
 
     @staticmethod
     def _loadExpr(dct):
         if dct['block'] == 'literal':
-            return Decoder._loadLiteral(dct)
+            typ = dct['type']
+            value = dct['value']
+
+            logging.debug('Finished Literal Block Load')
+            return blocks.LiteralBlock(typ,value)
+
 
         elif dct['block'] == 'variable':
-            return Decoder._loadVariable(dct)
+            ident = dct['ident']
+
+            logging.debug('Finished Variable Block Load')
+            return blocks.VariableBlock(ident)
 
         elif dct['block'] == 'binop':
-            return Decoder._loadBinOp(dct)
+            oper = dct['oper']
+            expr1 = dct['expr1']
+            expr2 = dct['expr2']
+
+            logging.debug('Finished BinOp Block Load')
+            return blocks.BinOpBlock(oper,expr1,expr2)
+
 
         elif dct['block'] == 'unop':
-            return Decoder._loadUnOp(dct)
+            oper = dct['oper']
+            expr1 = dct['expr1']
+
+            logging.debug('Finished UnOp Block Load')
+            return blocks.UnOpBlock(oper,expr1)
 
         else:
             logging.critical('Unknown expr block: ' + str(dct['block']))
@@ -139,13 +79,27 @@ class Decoder(json.JSONDecoder):
     @staticmethod
     def _loadStmt(dct):
         if dct['block'] == 'assignment':
-            return Decoder._loadAssignment(dct)
+            ident = dct['ident'] # Decoder._loadVariable(dct['ident'])
+            expr = dct['expr'] # Decoder._loadExpr(dct['expr'])
+
+            logging.debug('Finished Assignment Block Load')
+            return blocks.AssignmentBlock(ident,expr)
 
         elif dct['block'] == 'scope':
-            return Decoder._loadScope(dct)
+            stmts = dct['stmts'] # []
+            # for stmt in dct['stmts']:
+            #     stmts.append(Decoder._loadStmt(stmt))
+
+            logging.debug('Finished Scope Block Load')
+            return blocks.ScopeBlock(stmts)
 
         elif dct['block'] == 'program':
-            return Decoder._loadProgram(dct)
+            ident = dct['ident'] # Decoder._loadVariable(dct['ident'])
+            body = dct['body'] # Decoder._loadStmt(dct['body'])
+            param = None # Not implemented
+
+            logging.debug('Finished Program Block Load')
+            return blocks.ProgramBlock(ident,body,param)
 
         else:
             logging.critical('Unknown stmt block: ' + str(dct['block']))
