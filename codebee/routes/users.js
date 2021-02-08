c2075d4f46b25e55abb4485357724cd042555f7e
KrisYWD <74150813+KrisYWD@users.noreply.github.com>
2021-02-08 09:32:55 +0800

    Update users.js

--- codebee/routes/users.js
+++ codebee/routes/users.js
@@ -5,10 +5,16 @@ const User = require('../model/User')
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-router.post('/createuser', async(req, res) => {
+router.post('/', async(req, res) => {
     try{
       const {username,email,password} = req.body;
+      const user = await User.findOne({
+        email:email
+      });
+      if(user !== null){  res.json({result:1, name:""})}; //check if this email is registered, if yes return result as 1
+      const num = await User.find().sort({uid:-1}).limit(1); // get largest uid
       const uInfo = {
+        uid: num+1,
         name:username,
         email:email,
         password:password
@@ -18,23 +24,12 @@ router.post('/createuser', async(req, res) => {
       return res.json({
         message:"new user has been added;"
       })
+      res.json({result:0, name:username}); //send a successful message includes the name to be displayed
     }catch(e){
       return res.status(400).json({
         message:e.message,
       })
     }
 });
-
-
-/*
-request data {
-name : string,
-email: string,
-password: string,
-}
-*/
-// router.post('/createuser', (req, res) =>{
-//     res.json({result:0, name:""});
-// });
 
 module.exports = router;
