3ff77c80fabcef8ca96f64c08f55ca8063ac2890
KrisYWD <74150813+KrisYWD@users.noreply.github.com>
2021-02-08 09:35:05 +0800

    Update users.js

--- codebee/routes/users.js
+++ codebee/routes/users.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../model/User')
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
 router.post('/', async(req, res) => {
     try{
       const {username,email,password} = req.body;
@@ -21,15 +18,14 @@ router.post('/', async(req, res) => {
       }
       const newUser = new User(uInfo);
       await newUser.save();
-      return res.json({
-        message:"new user has been added;"
-      })
       res.json({result:0, name:username}); //send a successful message includes the name to be displayed
     }catch(e){
       return res.status(400).json({
         message:e.message,
       })
     }
+
 });
+
 
 module.exports = router;
