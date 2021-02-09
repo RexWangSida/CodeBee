5d99cbed9c9c68c2d4c4345872df8a06fb6f4bc9
wuyachen <1774288078@qq.com>
2021-02-09 15:00:50 -0500

    fix styling home page

--- codebee/src/components/Home.js
+++ codebee/src/components/Home.js
@@ -14,20 +14,20 @@ export default class Home extends Component {
             <HighlightedChar color="blue">C</HighlightedChar>odebee{" "}
           </h1>
         </div>
-        <main className="mt-5 mx-auto max-w-7xl px-4 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-          <div className="sm:text-center lg:text-left">
+        <main className="mt-5 mx-auto max-w-7xl px-4 items-center">
+          <div className="sm:text-center lg:text-center">
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-3xl">
               <span className="block xl:inline">Data to enrich your</span>
               <span className="block text-indigo-600 xl:inline">
                 online business
               </span>
             </h1>
-            <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+            <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl">
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
               lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
               fugiat aliqua.
             </p>
-            <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+            <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-center">
               <div className="rounded-md shadow">
                 <a
                   href="/level-selection"
