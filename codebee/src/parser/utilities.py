16ab1b468fbbd0d75f9039b346621d8171a11d74
nagyj2 <jaysun_n@hotmail.com>
2021-02-14 18:42:17 -0500

    utilities for constructing programs in Python

---
+++ codebee/src/parser/utilities.py
@@ -0,0 +1,35 @@
+# Testing utility functions
+
+from blocks import *
+
+def makeLiteral(value):
+    if type(value) == int:      return LiteralBlock('int', value)
+    elif type(value) == str:    return LiteralBlock('str', value)
+    elif type(value) == bool:   return LiteralBlock('bool',value)
+    return value
+
+def makeOp(oper,val1,val2=None):
+
+    if type(val1) in (int,str):
+        val1 = makeLiteral(val1)
+    if type(val2) in (int,str):
+        val2 = makeLiteral(val2)
+
+    if val2 == None:            return UnOpBlock(oper,val1)
+    else:                       return BinOpBlock(oper,val1,val2)
+
+def makeAssign(var1,val1):
+    if type(var1) == str:       var1 = makeVar(var1)
+
+    if type(val1) in (int,str,bool):
+        val1 = makeLiteral(val1)
+
+    return AssignmentBlock(var1,val1)
+
+def makeVar(name):
+    return VariableBlock(name)
+
+def makeProgram(name,body):
+    if type(name) == str:   name = makeVar(name)
+
+    return ProgramBlock(name,body)
