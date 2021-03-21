9de6bb9db0b05851e35812097e94104fdf350b22
Shiv <shivaanshseal@gmail.com>
2021-03-21 06:30:52 -0400

    Level 4 to be updated, needs testing

---
+++ codebee/src/Levels/Level_4_1.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import * as config from './config/Level_2_1.json';
+
+class Level_4_1 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            blockList: [], 
+            answer: config.answer, 
+            solution: [], 
+            achievementList: [], 
+            questionStatement: config.questionStatement,
+            hints: config.hints    
+        }
+    }
+
+    //TODO: Check how the solution will be stored. Might be another JSON object.
+    // This method should be called by the front end when the submit button is pressed.
+    achievementUpdate(){
+        achievement = {}
+        achievement.key = "Conditional Success";
+        achievement.value = "Completed the conditionals level!";
+        achievements = [];
+        achievements.push(achievement);
+        this.setState({
+            achievementList: achievements
+                })
+    }
+}
+export default Level_1_1
