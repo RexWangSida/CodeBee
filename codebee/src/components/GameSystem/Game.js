e14a7e6461db631957457747dd321fc9b0babd0f
Deuce-Cao <hq.cao1@gmail.com>
2021-03-23 16:16:54 -0400

    Add error message

--- codebee/src/components/GameSystem/Game.js
+++ codebee/src/components/GameSystem/Game.js
@@ -6,251 +6,196 @@ import Dropzone from './Dropzone';
 import { Link } from 'react-router-dom';
 import './Game.css';
 
-const GAME_DURATION = 1000 * 30; // 30 seconds
-
 const initialState = {
-  // we initialize the state by populating the bench with a shuffled collection of blocks
-  bench: shuffle(BLOCKS),
-  [ATTRS.VAR]: [],
-  [ATTRS.OP]: [],
-  [ATTRS.VAL]: [],
-  gameState: GAME_STATE.READY,
-  timeLeft: 0,
+    // we initialize the state by populating the bench with a shuffled collection of blocks
+    bench: shuffle(BLOCKS),
+    [ATTRS.VAR]: [],
+    [ATTRS.OP]: [],
+    [ATTRS.VAL]: [],
 };
 
 class Instruction extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      instruction: "A variable can be thought of as a container with a name. The container is uniquely named and can be assigned a value, which will then be stored as the contents of the container. The name can then be used to retrieve the contents of the container.\nThe retrieved data can be viewed, modified or used in other, more complex operations. As an example, a variable can be called \"num\" and assigned a value 5 through the code:\nnum = 5\nYour challenge here is to store the values 1 and 3 in two variables called \"a\" and \"b\". \nStore the result of adding the value in these two variables in a new variable called \"c\"."
+    constructor(props) {
+        super(props);
+        this.state = {
+            instruction: "A variable can be thought of as a container with a name. The container is uniquely named and can be assigned a value, which will then be stored as the contents of the container. The name can then be used to retrieve the contents of the container.\nThe retrieved data can be viewed, modified or used in other, more complex operations. As an example, a variable can be called \"num\" and assigned a value 5 through the code:\nnum = 5\nYour challenge here is to store the values 1 and 3 in two variables called \"a\" and \"b\". \nStore the result of adding the value in these two variables in a new variable called \"c\"."
+        }
     }
-  }
 
-  render() {
-    return (
-      <div className="left instruction">{this.state.instruction}</div>
-    );
-  }
+    render() {
+        return (
+            <div className="left instruction">{this.state.instruction}</div>
+        );
+    }
 }
 class Title extends React.Component {
-  render() {
-    return (
-      <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-3xl" style={{ margin: "20px auto" }}>
-        <span className="block xl:inline" >Level</span>
-        <span className="block text-indigo-600 xl:inline"> 1-1 </span>
-      </h1>
-    );
-  }
+    render() {
+        return (
+            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-3xl" style={{ margin: "20px auto" }}>
+                <span className="block xl:inline" >Level</span>
+                <span className="block text-indigo-600 xl:inline"> 1-1 </span>
+            </h1>
+        );
+    }
 }
 
 class Result extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showMessage: false
+    constructor(props) {
+        super(props);
+        this.state = {
+            showMessage: false,
+            answer: false
+        }
     }
-  }
-  onButtonClickHandler = () => {
-    this.setState({ showMessage: true });
-  };
+    onButtonClickHandler = () => {
+        // this.props.submit();
+        this.setState({ showMessage: true });
+        if (this.props.blocks.length != 0) {
+            this.setState({ answer: true });
+        }
+    };
 
-  render() {
-    return (
-      <>
-        {this.state.showMessage && <Complete />}
-        <div className="left result">{this.state.showMessage && <p>c = 3<br />You are correct!</p>}</div>
-        <div className='right' style={{ margin: "auto" }}>
-          <button className="py-3 px-5 font-semibold rounded-lg shadow-md text-white bg-indigo-600 hover:bg-indigo-700" onClick={this.onButtonClickHandler}>Submit</button>
-        </div>
-      </>
-    );
-  }
+    render() {
+        return (
+            <>
+                {this.state.showMessage && this.state.answer && <Complete />}
+                <div className="left result">{this.state.showMessage && this.state.answer && <p>c = 4<br />You are correct!</p>}{this.state.showMessage && !this.state.answer && <p>Unable to parse, please retry.</p>}</div>
+                <div className='right' style={{ margin: "auto" }}>
+                    <button className="py-3 px-5 font-semibold rounded-lg shadow-md text-white bg-indigo-600 hover:bg-indigo-700" onClick={this.onButtonClickHandler}>Submit</button>
+                    {this.state.showMessage && !this.state.answer && <button style={{ marginLeft: "30px" }} className="py-3 px-5 font-semibold rounded-lg shadow-md text-white bg-indigo-600 hover:bg-indigo-700" onClick={window.location.reload.bind(window.location)}>Restart</button>}
+                </div>
+            </>
+        );
+    }
 }
 
 class Complete extends React.Component {
 
-  render() {
-    return (
-      <>
-        <div className="popwindow">
-          <div className="windowtitle text-xl tracking-tight font-bold text-indigo-600">Level Completed</div>
-          <div className="windowcontent">
-            <div className="windowtext text-xl tracking-tight" >
-              Congratulations, you're completed this level.
-          </div>
-            <div className="windowtext">
-              <span className="text-xl tracking-tight">You have unlocked an achievement.</span><br />
-              <Link to="/achievements">
-                <button className="gamebutton py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-indigo-600 hover:bg-indigo-700">Check Achievement</button></Link>
-            </div>
-            <Buttons comp={true} />
-          </div>
-        </div>
-      </>
-    );
-  }
+    render() {
+        return (
+            <>
+                <div className="popwindow">
+                    <div className="windowtitle text-xl tracking-tight font-bold text-indigo-600">Level Completed</div>
+                    <div className="windowcontent">
+                        <div className="windowtext text-xl tracking-tight" >
+                            Congratulations, you're completed this level.
+                            </div>
+                        <div className="windowtext">
+                            <span className="text-xl tracking-tight">You have unlocked an achievement.</span><br />
+                            <Link to="/achievements">
+                                <button className="gamebutton py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-indigo-600 hover:bg-indigo-700">Check Achievement</button></Link>
+                        </div>
+                        <Buttons comp={true} />
+                    </div>
+                </div>
+            </>
+        );
+    }
 }
 
 
 class Buttons extends React.Component {
 
-  restart = () => {
-    window.location.reload();
-  }
-  render() {
-    return (
-      <div className="buttonGroup">
-        <Link to="/level-selection">
-          <button className="gamebutton py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-indigo-600 hover:bg-indigo-700">Return</button>
-        </Link>
-        <button className="gamebutton py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-indigo-600 hover:bg-indigo-700" onClick={this.restart}>Restart</button>
-        {this.props.comp ? <button className="gamebutton py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-indigo-600 hover:bg-indigo-700">Next</button> : <button className="gamebutton py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-indigo-600 hover:bg-indigo-700">Save</button>}
-      </div>
-    );
-  }
+    restart = () => {
+        window.location.reload();
+    }
+    render() {
+        return (
+            <div className="buttonGroup">
+                <Link to="/level-selection">
+                    <button className="gamebutton py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-indigo-600 hover:bg-indigo-700">Return</button>
+                </Link>
+                <button className="gamebutton py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-indigo-600 hover:bg-indigo-700" onClick={this.restart}>Restart</button>
+                {this.props.comp ? <Link to="/second"><button className="gamebutton py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-indigo-600 hover:bg-indigo-700">Next</button></Link> : <button className="gamebutton py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-indigo-600 hover:bg-indigo-700">Save</button>}
+            </div>
+        );
+    }
 }
 
 class Game extends React.Component {
-  state = initialState;
+    state = initialState;
 
-  onDragEnd = ({ source, destination }) => {
-    if (!destination) {
-      return;
+    onDragEnd = ({ source, destination }) => {
+        if (!destination) {
+            return;
+        }
+
+        this.setState(state => {
+            return move(state, source, destination);
+        });
+    };
+
+    submit = () => {
+        // var stms = []
+        // var blocks = this.state[ATTRS.PLAY];
+        // for (var i=0; i < blocks.length; i++) {
+        //   var block = blocks[i];
+        //   var stmt = {
+        //     "block": block.attr1,
+        //     "expr": {
+        //       "block": block.attr3,
+        //       "type": block.attr4,
+        //       "value": block.name.split[-1]
+        //     },
+        //     "ident": {
+        //       "block": block.attr2,
+        //       "ident": block.name.split[0]
+        //     }
+        //   }
+        //   stms.push(stmt);
+        // }
+        // console.log(stmt);
+        //need a fetch here to send to backend?
     }
 
-    this.setState(state => {
-      return move(state, source, destination);
-    });
-  };
+    render() {
+        const { gameState, timeLeft, bench, ...groups } = this.state;
+        const isDropDisabled = gameState === GAME_STATE.DONE;
 
-  submit = () => {
-
-    // var stms = []
-    // var blocks = this.state[ATTRS.PLAY];
-    // for (var i=0; i < blocks.length; i++) {
-    //   var block = blocks[i];
-    //   var stmt = {
-    //     "block": block.attr1,
-    //     "expr": {
-    //       "block": block.attr3,
-    //       "type": block.attr4,
-    //       "value": block.name.split[-1]
-    //     },
-    //     "ident": {
-    //       "block": block.attr2,
-    //       "ident": block.name.split[0]
-    //     }
-    //   }
-    //   stms.push(stmt);
-    // }
-    // console.log(stmt);
-    const code = {
-      "block": "program",
-      "body": {
-        "block": "scope",
-        "stmts": [
-          {
-            "block": "assignment",
-            "expr": {
-              "block": "literal",
-              "type": "int",
-              "value": "1"
-            },
-            "ident": {
-              "block": "variable",
-              "ident": "var1"
-            }
-          },
-          {
-            "block": "assignment",
-            "expr": {
-              "block": "literal",
-              "type": "int",
-              "value": "3"
-            },
-            "ident": {
-              "block": "variable",
-              "ident": "var2"
-            }
-          },
-          {
-            "block": "assignment",
-            "expr": {
-              "block": "binop",
-              "expr1": {
-                "block": "variable",
-                "ident": "var1"
-              },
-              "expr2": {
-                "block": "variable",
-                "ident": "var2"
-              },
-              "oper": "+"
-            },
-            "ident": {
-              "block": "variable",
-              "ident": "var3"
-            }
-          }
-        ]
-      },
-      "ident": {
-        "block": "variable",
-        "ident": "testProg"
-      }
-    }
-    //need a fetch here to send to backend?
-    console.log(code);
-  }
-
-  render() {
-    const { gameState, timeLeft, bench, ...groups } = this.state;
-    const isDropDisabled = gameState === GAME_STATE.DONE;
-
-    return (
-      <div className='canves'>
-        <div className='row'>
-          <Instruction />
-          <div className="right">
-            <Title />
-            <Buttons />
-          </div>
-        </div>
-        <DragDropContext onDragEnd={this.onDragEnd}>
-          <div className="row row-auto" style={{ height: "calc(100% - 408px)" }}>
-            <div className="left row block bg-indigo-200 rounded-lg p-4 border border-gray-200">
-              <Dropzone
-                pos="col"
-                id={ATTRS.VAR}
-                blocks={this.state[ATTRS.VAR]}
-                isDropDisabled={isDropDisabled}
-              />
-              <Dropzone
-                pos="col"
-                id={ATTRS.OP}
-                blocks={this.state[ATTRS.OP]}
-                isDropDisabled={isDropDisabled}
-              />
-              <Dropzone
-                pos="col"
-                id={ATTRS.VAL}
-                blocks={this.state[ATTRS.VAL]}
-                isDropDisabled={isDropDisabled}
-              />
+        return (
+            <div className='canves'>
+                <div className='row'>
+                    <Instruction />
+                    <div className="right">
+                        <Title />
+                        <Buttons />
+                    </div>
+                </div>
+                <DragDropContext onDragEnd={this.onDragEnd}>
+                    <div className="row row-auto" style={{ height: "calc(100% - 408px)" }}>
+                        <div className="left row block bg-indigo-200 rounded-lg p-4 border border-gray-200">
+                            <Dropzone
+                                pos="col"
+                                id={ATTRS.VAR}
+                                blocks={this.state[ATTRS.VAR]}
+                                isDropDisabled={isDropDisabled}
+                            />
+                            <Dropzone
+                                pos="col"
+                                id={ATTRS.OP}
+                                blocks={this.state[ATTRS.OP]}
+                                isDropDisabled={isDropDisabled}
+                            />
+                            <Dropzone
+                                pos="col"
+                                id={ATTRS.VAL}
+                                blocks={this.state[ATTRS.VAL]}
+                                isDropDisabled={isDropDisabled}
+                            />
+                        </div>
+                        <Dropzone
+                            pos="right"
+                            id="bench"
+                            blocks={bench}
+                            isDropDisabled={isDropDisabled} />
+                    </div>
+                </DragDropContext>
+                <div className='row' style={{ bottom: "0px" }}>
+                    <Result blocks={this.state[ATTRS.VAR]} submit={this.submit} />
+                </div>
             </div>
-            <Dropzone
-              pos="right"
-              id="bench"
-              blocks={bench}
-              isDropDisabled={isDropDisabled} />
-          </div>
-        </DragDropContext>
-        <div className='row' style={{ bottom: "0px" }}>
-          <Result />
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default Game;
